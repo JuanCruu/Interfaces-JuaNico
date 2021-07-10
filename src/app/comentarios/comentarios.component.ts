1b218935ee247768382eb4d25ffce02ec8cc7512
JuanCruu <juancruznavarro592@gmail.com>
2021-07-09 22:19:43 -0300

    se agrego funcionalidad comentario

--- src/app/comentarios/comentarios.component.ts
+++ src/app/comentarios/comentarios.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComentariosComponent implements OnInit {
 
+  idComentario:string="";
+
   constructor() { }
+  comentarios:{nombre:string,fecha:string,comentario:string}[]=[
+    {
+    nombre:"juan",
+    fecha:"febrero de 2020",
+    comentario:"uwu"
+  },
+  {
+    nombre:"carla",
+    fecha:"agosto de 2020",
+    comentario:"ayuda"
+  },
+  {
+    nombre:"karina",
+    fecha:"febrero de 202",
+    comentario:"uwu"
+  }
+
+]
 
   ngOnInit(): void {
+  }
+  changeInput(e: string){
+    this.idComentario=e;
+    // console.log(this.idComentario)
+  }
+  comentar():void{
+    let comentario=this.idComentario
+    let cosa={
+      nombre:"usuario1",
+      fecha:"julio 2021",
+      comentario:comentario
+    }
+  this.comentarios.push(cosa);
+    
   }
 
 }
